46e2c5038d529c2e3ab9701627f2eeb9b4f22c48
Amin <aminfathi80@gmail.com>
2023-09-22 16:12:41 +0330

    commenting system implemented

---
+++ src/controllers/commentsController.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from "express";
+import db from "../models";
+
+export async function getComments(req: Request, res: Response) {
+  const { postId } = req.params;
+  try {
+    const comments = await db.Comments.findAll({ where: { PostId: postId } });
+    return res.status(200).json(comments);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Something happened wrong. Try again." });
+  }
+}
+
+export async function createComment(req: Request, res: Response) {
+  const { postId } = req.params;
+  const { body } = req.body;
+  const userId = req.user.id;
+  try {
+    const post = await db.Posts.findByPk(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found." });
+    } else {
+      await db.Comments.create({ body: body, PostId: postId, UserId: userId });
+      return res.status(200).json({ message: "Successfuly commented." });
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Something happened wrong. Try again." });
+  }
+}
+
+export async function deleteComment(req: Request, res: Response) {
+  const { commentId } = req.params;
+  const userId = req.user.id;
+  try {
+    const comment = await db.Comments.findByPk(commentId);
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found." });
+    } else {
+      if (comment.UserId !== userId) {
+        return res.status(403).json({
+          message: "Access denied. Only author of this comment can delete it.",
+        });
+      } else {
+        await db.Comments.destroy({
+          where: { id: commentId, UserId: userId },
+        });
+        return res.status(200).json({
+          message: "Successfuly deleted the comment.",
+        });
+      }
+    }
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Something happened wrong. Try again." });
+  }
+}
--- src/routes/comments/index.ts
+++ src/routes/comments/index.ts
@@ -1,5 +1,15 @@
+import {
+  getComments,
+  createComment,
+  deleteComment,
+} from "../../controllers/commentsController";
+import { authenticate } from "../../middlewares/authMiddleware";
 import express from "express";
 
 const router = express.Router();
+
+router.get("/by-post-id/:postId", getComments);
+router.post("/by-post-id/:postId", authenticate, createComment);
+router.delete("/:commentId", authenticate, deleteComment);
 
 export { router as commentsRouter };
